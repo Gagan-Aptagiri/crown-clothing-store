6f60bd4a6d42062eb63ddfde2d67a60f38f2b218
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-04 18:59:31 +0530

    Added sign-in-sign-out page and SignIn component

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './sign-in-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInSignUp = () => {
+	return (
+		<div className="sign-in-sign-up">
+			<SignIn />
+			<div>SignUp</div>
+		</div>
+	);
+};
+
+export default SignInSignUp;
