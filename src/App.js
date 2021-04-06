31f5cd446e27d2007a663cdadc0f2eb8a7cf661b
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-06 22:14:57 +0530

    Added redirect from signInPage if user is signedIn and mappedStateToProps for App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -44,12 +44,24 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
-					<Route path="/signin" component={SignInSignUp} />
+					<Route
+						exact
+						path="/signin"
+						render={() =>
+							this.props.currentUser ? <Redirect to="/" /> : <SignInSignUp />
+						}
+					/>
 				</Switch>
 			</div>
 		);
 	}
 }
+
+const mapStateToProps = ({ user }) => {
+	return {
+		currentUser: user.currentUser,
+	};
+};
 
 const mapDispatchToProps = (dispatch) => {
 	return {
@@ -57,4 +69,4 @@ const mapDispatchToProps = (dispatch) => {
 	};
 };
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
