7e6693ae0a1b80e5ac9135b14b4adc31b188d779
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-06 23:01:33 +0530

    Added CartDropdown component and fixed app styling

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => {
@@ -31,6 +32,7 @@ const Header = ({ currentUser }) => {
 				)}
 				<CartIcon />
 			</div>
+			<CartDropdown />
 		</div>
 	);
 };
