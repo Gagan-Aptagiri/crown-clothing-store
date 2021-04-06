f61a9714fa5ef780f22ca9ab223df030bab13959
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-06 20:21:06 +0530

    connected header component to redux store

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -32,4 +33,10 @@ const Header = ({ currentUser }) => {
 	);
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+	return {
+		currentUser: state.user.currentUser,
+	};
+};
+
+export default connect(mapStateToProps)(Header);
