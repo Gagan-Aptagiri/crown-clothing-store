01534ed36b6cf7a21c4641a4833a3ff9475392e2
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-04-09 18:00:51 +0530

    Added CategoryPage Component, added initial <Route /> setup for the ShopPage component

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collections-item/collections-item.component';
+import './category.styles.scss';
+
+const CategoryPage = ({ match }) => {
+	return (
+		<div className="category">
+			<h2>Category Page</h2>
+		</div>
+	);
+};
+
+export default CategoryPage;
